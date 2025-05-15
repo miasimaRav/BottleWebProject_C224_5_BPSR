f462b6f0405b472f74832e60e5d05bf9a68c06f3
Vasilisa <syapchik@gmail.com>
2025-05-15 14:48:15 +0300

    Работа над главным меню сайта. Пробная версия 1.

--- BottleWebProject_C224_5_BPSR/app.py
+++ BottleWebProject_C224_5_BPSR/app.py
@@ -5,6 +5,8 @@ import sys
 
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
+from bottle import Bottle, static_file
+
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
@@ -27,10 +29,7 @@ if __name__ == '__main__':
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
+        return static_file(filepath, root='./static')
 
     # Starts a local test server.
     bottle.run(server='wsgiref', host=HOST, port=PORT)
